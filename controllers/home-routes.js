c252bab253a1cf5a0550058f156f808ef02d1899
Mathew B <bogaert@email.arizona.edu>
2021-01-20 11:05:52 -0700

    added signup, direct to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,6 +41,9 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/signup", (req, res) => {
+  res.render('signup');
+});
 
 router.get("/post/:id", (req, res) => {
   Post.findOne({
